0f7d8572366928043ca450d3653ed1dbfd642de0
Bonet Sugiarto <bonetsu@gmail.com>
2018-01-15 01:34:33 +0800

    Initial commit

---
+++ src/components/ProductsList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="blah">
+    <table>
+      <tr>
+        <td>message</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductsList',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
