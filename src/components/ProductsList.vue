3bb887f1c8dcd3947f40b6f94da8d0fda5bed4df
Bonet Sugiarto <bonetsu@gmail.com>
2018-01-20 06:00:40 +0800

    Include ElementUI

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="blah">
-    <table>
-      <tr>
-        <td>message</td>
-      </tr>
-    </table>
+  <div class="demo-layout">
+    <el-row>
+      <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+      <el-col :span="12">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+    </el-row>
+    <el-button>Test</el-button>
+    <el-button type="primary" :loading="true">Loading…</el-button>
+    <el-pagination v-bind:current-page="5" v-bind:page-size="100" v-bind:total="1000" layout="prev, pager, next"></el-pagination>
+
   </div>
 </template>
 
@@ -21,18 +37,31 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.el-row {
+  margin-bottom: 20px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.el-row:last-child {
+  margin-bottom: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.el-col {
+  border-radius: 4px;
 }
-a {
-  color: #42b983;
+.bg-purple-dark {
+  background: #99a9bf;
 }
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+
 </style>
